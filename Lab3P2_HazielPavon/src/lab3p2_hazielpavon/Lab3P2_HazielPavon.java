67d1808b9ba6b0b8da540e7f8b4370fa11ed52d1
Hazielpavon <131502216+Hazielpavon@users.noreply.github.com>
2023-08-04 13:38:56 -0600

    Add files via upload

---
+++ Lab3P2_HazielPavon/src/lab3p2_hazielpavon/Lab3P2_HazielPavon.java
@@ -0,0 +1,54 @@
+package lab3p2_hazielpavon;
+
+import java.util.Scanner;
+
+public class Lab3P2_HazielPavon {
+
+    public static void main(String[] args) {
+
+        Scanner leer = new Scanner(System.in);
+
+        boolean seguir = true;
+
+        while (seguir) {
+
+            System.out.println("1. Sistema de vehiculos");
+            System.out.println("2. Salir");
+            int opcion = leer.nextInt();
+
+            switch (opcion) {
+
+                case 1:
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+
+                    break;
+
+                case 2:
+
+                    seguir = false;
+                    System.out.println("Saliendo");
+
+                    break;
+
+                default:
+
+                    System.out.println("Ingrese una opcion correcta");
+
+            }
+
+        }
+
+    }
+
+}
